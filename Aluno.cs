3e3940b2e2dd5fe913343111188572c87a6c134b
Diego Boschetti <dlboschetti@gmail.com>
2020-07-15 01:40:13 -0300

    first commit

---
+++ Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace student_class
+{
+    class Aluno
+    {
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Sexo { get; set; }
+        public string Periodo { get; set; }
+        public string Serie { get; set; }
+        public double mediaNota;
+
+        public void AplicarNota(int nota1, int nota2)
+        {
+               mediaNota = (nota1 + nota2)/2;  
+        }
+
+        public void  ImprimirNota()
+        {
+            Console.WriteLine($"A média da(o) Aluna(o) {Nome} da série {Serie} periodo {Periodo} foi de {mediaNota}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace student_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno aluno = new Aluno();
+
+            aluno.Nome = "Lucas Moura Boschetti";
+            aluno.Idade = 4;
+            aluno.Periodo = "Tarde";
+            aluno.Serie = "Jardim I";
+            aluno.Sexo = "Masculino";
+
+            Console.WriteLine($"informar notas do aluno {aluno.Nome} :");
+            Console.WriteLine("Nota1:");
+            int nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nota2:");
+            int nota2 = Convert.ToInt32(Console.ReadLine());
+            aluno.AplicarNota(nota1, nota2);
+
+            aluno.ImprimirNota();
+            
+        }
+    }
+}
